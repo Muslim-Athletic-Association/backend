599358f064531a55d0a52a70eadf0b05a582c046
Khalid Radwan <khalidradwan@user3-239-226.wireless.utoronto.ca>
2021-01-06 17:40:00 -0500

    registeration and login verification

--- model/person.js
+++ model/person.js
@@ -1,3 +1,4 @@
+const { setResult } = require("./constants");
 const c = require("./constants");
 
 /**
@@ -18,21 +19,44 @@ async function createPerson(data) {
         phone: "phone",
         gender: "bool",
         birthday: "date",
+        password: "string"
     })
     if (invalid) {
         return invalid;
     }
-    var sql = 'INSERT INTO person (first_name, last_name, email, phone, gender, birthday) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;';
-    var params = [data.first_name, data.last_name, data.email, data.phone, data.gender, data.birthday]
+    var sql = 'INSERT INTO person (first_name, last_name, email, phone, gender, birthday, password) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;';
+    var params = [data.first_name, data.last_name, data.email, data.phone, data.gender, data.birthday, data.password]
     var m = new c.Message({
         success: "Successfully added a person.",
         duplicate: "A person with email already exists."
     });
     return await c.create(sql, params, m);
 }
-
+/**
+ * Uses the remove operation from ./constants in order to remove a program from the database.
+ * 
+ * @param {name: string} data 
+ */
+async function getPerson(data) {
+    console.log(data);
+    var sql = 'SELECT * from person where email = $1;';
+    var params = [data.email];
+    var m = new c.Message({
+        success: "Successfully retrieved user matching email."
+    });
+    return await c.retrieve(sql, params, m).then(async function (result) {
+        var password = result.data[0].password;
+        if(password != data.password){
+            return setResult({}, false, "Incorrect password", c.errorEnum.INVALID)
+        } else {
+            delete result.data.password;
+            return result;
+        }
+    });
+}
 module.exports = {
     createPerson: createPerson,
+    getPerson: getPerson
 }
 
 // module.exports = {
