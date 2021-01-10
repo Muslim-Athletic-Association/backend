f01227682a3c66173f6d9e150db7df004f2ac5e0
Osama <osama.hafez@mail.utoronto.ca>
2021-01-10 12:16:38 -0500

    Adding temporary registration measures, logic in modelling and routes changed.

--- routes/registration.js
+++ routes/registration.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const p = require("../model/registration");
-const c = require("./routingConstants");
+const r = require("../model/registration");
+const p = require("../model/person");
+const c = require("../model/constants");
+const rc = require("./routingConstants");
 
 /**
  * Add a program POST request handling.
@@ -25,8 +27,8 @@ const c = require("./routingConstants");
 router.post('/api/registration/subscribe', async function createMemberResponse(request, response) {
     // returns member information in json format if successful
     response.header('Access-Control-Allow-Origin', '*');
-    await p.subscribe(request.body).then(async function (result) {
-        return await c.simpleResponse(result, response);
+    await r.subscribe(request.body).then(async function (result) {
+        return await rc.simpleResponse(result, response);
     });
 })
 
@@ -34,8 +36,25 @@ router.get('/api/registration/getPrograms/:person', async function createMemberR
     // returns member information in json format if successful
     response.header('Access-Control-Allow-Origin', '*');
     console.log(request.params)
-    await p.getPrograms(request.params).then(async function (result) {
-        return await c.simpleResponse(result, response);
+    await r.getPrograms(request.params).then(async function (result) {
+        return await rc.simpleResponse(result, response);
+    });
+})
+
+router.post('/api/registration/temporary/subscribe', async function createMemberResponse(request, response) {
+    // returns member information in json format if successful
+    response.header('Access-Control-Allow-Origin', '*');
+    var subscribe_body = request.body;
+    await p.createPerson(request.body).then(async function (result) {
+        if (result.ecode == c.errorEnum.UNIQUE) {
+            result = await p.getPerson(request.body).then((result) => { return result });
+        }
+        await r.subscribe({ ...request.body, person: result.data[0].person_id }).then(async function (result2) {
+            if(result2.success){
+                result2.error = "Successfully registered for this program."
+            }
+            return await rc.simpleResponse(result2, response);
+        });
     });
 })
 
