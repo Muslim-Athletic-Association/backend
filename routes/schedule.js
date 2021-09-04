f4bb330718e4dfe257fa0c54e8ecd0730e6a4be0
Osama <osama.hafez@mail.utoronto.ca>
2021-09-04 18:38:27 -0400

    temp route added for schedule creation

--- routes/schedule.js
+++ routes/schedule.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const c = require("../model/constants");
 const rc = require("./routingConstants");
 const fixtures = require("../model/fixture");
+const schedule = require("../model/schedule");
 
 router.post(
     "/api/upload/fixture",
@@ -31,6 +32,25 @@ router.get(
         // fetch fixtures from the database based on group (or group and date).
         await fixtures
             .getGroupFixtures(request.params)
+            .then(async function (result) {
+                return await rc.simpleResponse(result, response);
+            });
+    }
+);
+
+router.post(
+    "/api/createSchedule",
+    async function createFixtures(request, response) {
+        // Create a schedule based on a list of team names.
+        // Step 1: Sanitize the list of team names
+        // Step 2: Ensure that no schedule already exists for this group
+        //  i.e. cannot generate a new schedule for a group that already contains a fixture.
+
+
+        // Step 3: Create schedule using generate_matches() in schedule model
+        //  ^To be tested first.
+        await schedule
+            .generateMatches(request.body)
             .then(async function (result) {
                 return await rc.simpleResponse(result, response);
             });
