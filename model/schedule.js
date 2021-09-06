9b95199cfc7da26b8da7366fc5a4b5653a1e452a
Osama <osama.hafez@mail.utoronto.ca>
2021-09-06 17:54:02 -0400

    fixed date/time formatting and update

--- model/schedule.js
+++ model/schedule.js
@@ -4,7 +4,7 @@ const c = require("./constants");
 const isFunction = c.isFunction;
 const errorEnum = c.errorEnum;
 
-// NOTE: handle edge cases as they come along, not all at once
+// TODO: Lots of edge cases to handle. Handle them as they come along, not all at once.
 
 moment().format();
 
@@ -26,8 +26,8 @@ class Match {
         this.team1 = team1;
         this.team2 = team2;
         this.cgroup = cgroup;
-        this.fixture_date = fixture_date;
-        this.fixture_time = fixture_time;
+        this.date = fixture_date;
+        this.time = fixture_time;
         this.score1 = score1;
         this.score2 = score2;
     }
@@ -37,15 +37,11 @@ class Match {
      * @return {Object} database insertion result
      */
     async create() {
+        let date = this.date ? this.date.format("YYYY-MM-DD") : null;
+        let time = this.time ? this.time.format("HH:MM:SS") : null;
         let sql =
             "INSERT INTO fixture (team1, team2, cgroup, fixture_date, fixture_time) VALUES ($1, $2, $3, $4, $5) RETURNING *;";
-        let params = [
-            this.team1,
-            this.team2,
-            this.cgroup,
-            this.date,
-            this.time,
-        ];
+        let params = [this.team1, this.team2, this.cgroup];
         let msg = new c.Message({
             success: `Created match \n ${this.toString()}`,
             duplicate: `A Match already exists on ${this.date} or ${this.time} for one of ${this.team1} or ${this.team2}.`,
@@ -59,7 +55,7 @@ class Match {
      * now we will update the matches in the actual database.
      * @return {Object} result from database
      */
-    async update(match) {
+    async update() {
         let old = this.toString();
         let sql =
             "UPDATE fixture SET team1 = $1, team2 = $2, cgroup = $3, fixture_date = $4, fixture_time = $5 RETURNING *;";
@@ -67,15 +63,16 @@ class Match {
             this.team1,
             this.team2,
             this.cgroup,
-            this.date,
-            this.time,
+            this.date.format("YYYY-MM-DD"),
+            this.time.format("HH:MM:SS"),
         ];
         let msg = new c.Message({
             success: `Updated match \n old: ${old} \n new: ${this.toString()}`,
             duplicate: `A Match already exists on ${this.date} or ${this.time} for one of ${this.team1} or ${this.team2}.`,
-            foreign: `Either team: ${team1} or ${team2} or group: ${cgroup} does not exist.`,
+            foreign: `Either team: ${this.team1} or ${this.team2} or group: ${this.cgroup} does not exist.`,
         });
-        await c.update(sql, params, msg);
+        let falafel = await c.update(sql, params, msg);
+        console.log(falafel);
     }
 
     /**
@@ -85,8 +82,7 @@ class Match {
      */
     isLaterThan(other) {
         return (
-            this.fixture_date >= other.fixture_date &&
-            this.fixture_time > other.fixture_time
+            this.date >= other.fixture_date && this.time > other.fixture_time
         );
     }
 
@@ -97,8 +93,7 @@ class Match {
      */
     sameDayAndTimeAs(other) {
         return (
-            this.fixture_date == other.fixture_date &&
-            this.fixture_time == other.fixture_time
+            this.date == other.fixture_date && this.time == other.fixture_time
         );
     }
 
@@ -108,7 +103,7 @@ class Match {
      * @returns {bool}
      */
     sameDayAs(other) {
-        return this.fixture_date == other.fixture_date;
+        return this.date == other.fixture_date;
     }
 
     /**
@@ -117,7 +112,7 @@ class Match {
      * @returns {bool}
      */
     sameTimeAs(other) {
-        return this.fixture_date == other.fixture_date;
+        return this.date == other.fixture_date;
     }
 
     /**
@@ -153,12 +148,13 @@ class Match {
      */
     setDateTime(t) {
         let datetime = t.toJSON().split(":");
-        this.fixture_time = datetime[1];
-        this.fixture_date = datetime[0];
+        this.time = new moment(datetime[1]);
+        this.date = new moment(datetime[0]);
+        this.update();
     }
 
     toString() {
-        return `${this.team1} | ${this.score1}:${this.score2} | ${this.team2} at ${this.fixture_date} on ${this.fixture_time} in group ${this.cgroup}`;
+        return `${this.team1} | ${this.score1}:${this.score2} | ${this.team2} at ${this.date} on ${this.time} in group ${this.cgroup}`;
     }
 
     /**
@@ -191,7 +187,7 @@ class Schedule {
     async addTeam(team1) {
         let num_teams = this.teams.length;
         for (var i = 0; i < num_teams; i++) {
-            let team2 = this.teams[i]
+            let team2 = this.teams[i];
             let match = new Match(team1, team2, this.group);
             await match.create();
             this.matches.push(match);
@@ -209,29 +205,34 @@ class Schedule {
      * @param {int} numFields the number of fields booked at the location
      * @param {int} matchLength based on the number of hours, this is how long a game is (can be float)
      */
-    async weekdaySchedule(startTime, endTime, startDate, numFields, matchLength) {
+    async weekdaySchedule(
+        startTime,
+        endTime,
+        startDate,
+        numFields,
+        matchLength
+    ) {
         var duration = moment.duration(endTime.diff(startTime));
         var bookingLength = duration.asHours();
-        let numDays = Math.floor(this.matches.length / (bookingLength * matchLength)) + 1
-        let endDate = new moment(startDate);
+        let numDays =
+            Math.floor(this.matches.length / (bookingLength * numFields)) + 1;
+        let endDate = new moment(startDate).add(numDays, "weeks");
         for (var date = startDate; date < endDate; date.add(1, "weeks")) {
             this.dates[date] = [];
             this.prepareMatchDay(
                 startTime,
                 endTime,
                 date,
-                numFields
+                numFields,
+                matchLength
             );
-            // Note: what if a date is already booked?
-            // We have a conflict and should move all matches on that day to another day.
         }
-        for (var date = 0; date < numDays; date.add(1, "weeks")) {
-            let matches = this.dates[date];
-            for (var match = 0; match < matches.length; match++) {
-                await matches[match].update();
-            }
-        }
-        return c.setResult({"blah":"coolio"}, true, "Falafel", c.errorEnum.NONE)
+        return c.setResult(
+            { blah: "coolio" },
+            true,
+            "Falafel",
+            c.errorEnum.NONE
+        );
     }
 
     /**
@@ -247,15 +248,18 @@ class Schedule {
      * @param {int} numFields the number of fields booked at the location
      * @param {int} matchLength number of hours, this is how long a game is (can be float)
      */
-    prepareMatchDay(startTime, endTime, date, numFields, bookingLength, matchLength) {
+    prepareMatchDay(startTime, endTime, date, numFields, matchLength) {
         let scheduled = [];
         let current;
         let m = this.matches.length - 1;
         for (var t = startTime; t < endTime; t.add(matchLength, "hours")) {
-            for (var f = numFields; (f > 0) && (m > 0); f--) {
+            // Keep scheduling matches on this day until the booked time runs out
+            console.log("??");
+            for (var f = numFields; f > 0 && m > 0; f--) {
                 // Keep scheduling games at this matchLength until the fields run out
                 current = this.matches[m];
                 m--;
+                console.log("!!");
                 current.setDateTime(t);
                 scheduled.push(current);
                 this.dates[date].push(current);
@@ -278,7 +282,6 @@ class Schedule {
         });
         this.dates[nDate] = matches;
         delete this.dates[date];
-        // TODO: Update matches in database
     }
 
     /**
@@ -294,17 +297,6 @@ class Schedule {
      */
     deleteMatch(match) {}
 }
-
-/**
- * Givens:
- *  - A list of teams  names(and therefore the # of teams)
- *
- *
- * We want to:
- *  - pair all of the teams (create a fixture)
- *  - Add a time to each game (make sure each time does not conflict with the time of another team)
- *      - Make sure each team plays every other team at least once.
- */
 
 /**
  *
