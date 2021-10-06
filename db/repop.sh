8ab1ef62f6b8f214b391f09571b786a5cad67817
osama <osama.w.hafez@gmail.com>
2021-10-06 19:05:00 -0400

    rename .ddl to .sql

--- db/repop.sh
+++ db/repop.sh
@@ -1,6 +1,6 @@
 #This file should be executed in it's local directory
-psql postgres < ./drop.ddl
-psql maadmin < ./schema.ddl
-psql maadmin < ./soccer21.ddl
-psql maadmin < ./views.ddl
+psql postgres < ./drop.sql
+psql maadmin < ./schema.sql
+psql maadmin < ./soccer21.sql
+psql maadmin < ./views.sql
 
