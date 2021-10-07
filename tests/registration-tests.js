0d244173fbf8bdfd586070d24e17cb019a244695
osama <osama.w.hafez@gmail.com>
2021-10-07 10:13:34 -0400

    add utils for db and api requests, fix up db setup in setup.js

--- tests/registration-tests.js
+++ tests/registration-tests.js
@@ -1,6 +1,7 @@
+const require
+
 export function registration_tests() {
-    
-    
+        
     
     it("test individual registration for program", async () => {
         // const resp1 = await apiGET(`/admin/sessions/${session.id}/positions`);
